230c6c78dc1eb84b67fe7e33573ac2c352c38415
Bala Aravind <3aravidarba@gmail.com>
2023-01-28 15:52:32 +0530

    Deploy

---
+++ demo/src/main/java/com/example/demo/controller/ControllerClass.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+	
+	@GetMapping("/login")
+		public String loginpage() {
+		return "Welcome Bala";
+	}
+
+}
