0f3e4a0fcf8a2a875dd51b49d6df3810ac3d4ffc
Bala Aravind <121819501+Aravindarba@users.noreply.github.com>
2023-01-30 12:10:13 +0530

    h

--- demo/src/main/java/com/example/demo/controller/ControllerClass.java
+++ demo/src/main/java/com/example/demo/controller/ControllerClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerClass {
 	
-	@GetMapping("/login")
+	@GetMapping("/")
 		public String loginpage() {
 		return "Welcome Bala";
 	}
